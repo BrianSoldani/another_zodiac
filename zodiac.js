cf7f42c0c549d0eb4eee73a53cae5f221a068c39
BSoldi <briansoldani@gmail.com>
2020-06-19 13:29:33 -0700

    add old horoscope api test

--- zodiac.js
+++ zodiac.js
@@ -8,7 +8,7 @@ var createRow = function(data) {
 
     // Methods run on jQuery selectors return the selector they we run on
     // This is why we can create and save a reference to a td in the same statement we update its text
-    var titleTd = $("<td>").text(data);
+    var titleTd = $("<td>").text(data.name);
     var yearTd = $("<td>").text(data.compatibility);
     var iconTd = $("<td>").text(data.ruling_planet);
     var actorsTd = $("<td>").text(data.good_traits);
@@ -25,19 +25,36 @@ var createRow = function(data) {
     $("")
   };
 
-  // The search OMDB function takes a movie, searches the omdb api for it, and then passes the data to createRow
-//   var gogetit = function() {
-//     var queryURL = "http://numbersapi.com/02/13/date";
-//     console.log(queryURL);
-//     $.ajax({
-//       url: queryURL,
-//       method: "GET"
-//     }).then(function(response) {
-//       createRow(response);
-//     });
-//   };
+
+  var gogetit = function() {
+    var queryURL = "https://zodiacal.herokuapp.com/aries";
+    console.log(queryURL);
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+      dataType: "json",
+      headers: {
+        "origin,x-requested-with": "xhr"}
+    }).then(function(response) {
+      createRow(response);
+    });
+  };
   
-//   gogetit();
+  gogetit();
+
+  // The search OMDB function takes a movie, searches the omdb api for it, and then passes the data to createRow
+  // var gogetit = function() {
+  //   var queryURL = "http://numbersapi.com/02/13/date";
+  //   console.log(queryURL);
+  //   $.ajax({
+  //     url: queryURL,
+  //     method: "GET"
+  //   }).then(function(response) {
+  //     createRow(response);
+  //   });
+  // };
+  
+  // gogetit();
 
 
 // var d = new Date(1974, 04, 16);
@@ -50,12 +67,13 @@ var createRow = function(data) {
 // _calculateAge(d);
 // console.log(_calculateAge(d))
 
-const proxyurl = "https://cors-anywhere.herokuapp.com/";
-const url = "http://numbersapi.com/02/13/date"; // site that doesn’t send Access-Control-*
-fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
-.then(response => response.text())
-.then(contents => console.log(contents))
-.catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+// const proxyurl = "https://cors-anywhere.herokuapp.com/";
+// const url = "http://numbersapi.com/02/13/date"; // site that doesn’t send Access-Control-*
+// fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
+// .then(response => response.text())
+// .then(contents => console.log(contents))
+// .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+// // createRow(response.text());
 
 
 const url2 = "https://zodiacal.herokuapp.com/aries"; // site that doesn’t send Access-Control-*
