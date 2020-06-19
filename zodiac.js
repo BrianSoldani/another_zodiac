a4241e72327d5d6c581ff3a1c4afb0623f1d8aa1
BSoldi <briansoldani@gmail.com>
2020-06-19 00:21:48 -0700

    new update

--- zodiac.js
+++ zodiac.js
@@ -57,9 +57,9 @@ fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
 .then(contents => console.log(contents))
 .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
 
-const proxyurl = "https://cors-anywhere.herokuapp.com/";
-const url = "https://zodiacal.herokuapp.com/aries"; // site that doesn’t send Access-Control-*
-fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
+
+const url2 = "https://zodiacal.herokuapp.com/aries"; // site that doesn’t send Access-Control-*
+fetch(proxyurl + url2) // https://cors-anywhere.herokuapp.com/https://example.com
 .then(response => response.text())
 .then(contents => console.log(contents))
 .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
